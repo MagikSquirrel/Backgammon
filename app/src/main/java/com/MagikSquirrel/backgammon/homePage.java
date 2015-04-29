f5a50066a8c7b6662b653dc8e498da7f667034ad
MagikSquirrel <virgil_90210@yahoo.com>
2015-04-29 10:39:05 -0400

    Forgot to attach homePage changes

--- app/src/main/java/com/MagikSquirrel/backgammon/homePage.java
+++ app/src/main/java/com/MagikSquirrel/backgammon/homePage.java
@@ -1,6 +1,7 @@
 package com.MagikSquirrel.backgammon;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -15,42 +16,60 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
+import android.widget.Toast;
 
 public class homePage extends ActionBarActivity {
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_home_page);
 
         final LinearLayout llPiece = new LinearLayout(this);
         llPiece.layout(0,0,100,100);
         llPiece.setLayoutParams(new ActionBar.LayoutParams(100, 100));
         llPiece.setOrientation(LinearLayout.HORIZONTAL);
 
+        //NEW GAME BUTTON
         Button btnCreate = (Button) findViewById(R.id.bNewGame);
         btnCreate.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
 
-                final EditText etBlack = (EditText) findViewById(R.id.etBlackName);
-                etBlack.setText("Shit kinda worked!");
+                mContext = getApplicationContext();
 
+                Toast.makeText(mContext, "New Game Started", Toast.LENGTH_LONG).show();
 
+                ImageView ivBlack = new ImageView(homePage.this);
+                ivBlack.setLayoutParams(new GridView.LayoutParams(85, 85));
+                ivBlack.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                ivBlack.setPadding(8, 8, 8, 8);
+                ivBlack.setImageResource(R.drawable.pieceblack);
+                ivBlack.setVisibility(View.VISIBLE);
 
-                Drawable drawable = getResources().getDrawable(R.drawable.pieceblack);
-                //ivPic.setImageDrawable(drawable); // set the image to the ImageView
-                //llPiece.addView(ivPic);
-                //setContentView(llPiece);
-
-
-
+                GridView gBoard = (GridView) findViewById(R.id.gBoard);
+                gBoard.addView(ivBlack);
             }
         });
 
+        //CLEAR BUTTON
+        Button btnClear = (Button) findViewById(R.id.bClear);
+        btnClear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                final EditText etBlack = (EditText) findViewById(R.id.etBlackName);
+                etBlack.setText("");
+
+                final EditText etWhite = (EditText) findViewById(R.id.etWhiteName);
+                etWhite.setText("");
+            }
+        });
 
     }
 
